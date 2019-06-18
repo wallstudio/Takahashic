d2839e1892811db0cf7caa7190757bee339c41a1
wallstudio <yukawallstudio@gmail.com>
2019-06-19 01:37:44 +0900

    iroiro

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ function main(param: g.GameMainParameterObject): void {
 	const scene = new g.Scene({
 		game: g.game,
 		// このシーンで利用するアセットのIDを列挙し、シーンに通知します
-		assetIds: ["player", "shot", "se"]
+		assetIds: ["player", "shot", "se", "rock", "se2"]
 	});
 	scene.loaded.add(() => {
 		// ここからゲーム内容を記述します
@@ -14,50 +14,73 @@ function main(param: g.GameMainParameterObject): void {
 			width: (scene.assets["player"] as g.ImageAsset).width,
 			height: (scene.assets["player"] as g.ImageAsset).height
 		});
-
-		// プレイヤーの初期座標を、画面の中心に設定します
-		player.x = (g.game.width - player.width) / 2;
-		player.y = (g.game.height - player.height) / 2;
-		player.update.add(() => {
-			// 毎フレームでY座標を再計算し、プレイヤーの飛んでいる動きを表現します
-			// ここではMath.sinを利用して、時間経過によって増加するg.game.ageと組み合わせて
-			player.y = (g.game.height - player.height) / 2 + Math.sin(g.game.age % (g.game.fps * 10) / 4) * 10;
-
-			// プレイヤーの座標に変更があった場合、 modified() を実行して変更をゲームに通知します
+		let playerVel = 0;
+		player.x = 10;
+		player.y = g.game.height - player.height;
+		player.update.add(() => 
+		{
+			playerVel -= 0.5;
+			player.y -= playerVel;
+			if(player.y > g.game.height - player.height)
+			{
+				playerVel = 0;
+				player.y = g.game.height - player.height;
+			}
 			player.modified();
 		});
-
-		// 画面をタッチしたとき、SEを鳴らします
-		scene.pointDownCapture.add(() => {
-			(scene.assets["se"] as g.AudioAsset).play();
-
-			// プレイヤーが発射する弾を生成します
-			const shot = new g.Sprite({
-				scene: scene,
-				src: scene.assets["shot"],
-				width: (scene.assets["shot"] as g.ImageAsset).width,
-				height: (scene.assets["shot"] as g.ImageAsset).height
-			});
-
-			// 弾の初期座標を、プレイヤーの少し右に設定します
-			shot.x = player.x + player.width;
-			shot.y = player.y;
-			shot.update.add(() => {
-				// 毎フレームで座標を確認し、画面外に出ていたら弾をシーンから取り除きます
-				if (shot.x > g.game.width) shot.destroy();
-
-				// 弾を右に動かし、弾の動きを表現します
-				shot.x += 10;
-
-				// 変更をゲームに通知します
-				shot.modified();
-			});
-			scene.append(shot);
-		});
+		player.update.add(() => 
+		{
+			if(Math.random() < 0.001)
+			{
+				let rock = createRock(scene);
+				rock.update.add(() => 
+				{
+					const distLimit = 70
+					let distX = Math.abs(player.x - rock.x);
+					let distY = Math.abs(player.y - rock.y);
+					let sqrtMag = Math.pow(distX, 2) + Math.pow(distY, 2)
+					
+					if(sqrtMag < Math.pow(distLimit, 2))
+					{
+						scene.remove(player);
+						(scene.assets["se2"] as g.AudioAsset).play();
+					}
+				})
+			}
+		})
 		scene.append(player);
-		// ここまでゲーム内容を記述します
+
+		
+		scene.pointDownCapture.add(() => 
+		{
+			(scene.assets["se"] as g.AudioAsset).play();
+			playerVel += 15;
+		});
 	});
 	g.game.pushScene(scene);
+}
+
+function createRock(scene: g.Scene) : g.Sprite
+{
+	const rock = new g.Sprite({
+		scene : scene,
+		src : scene.assets["rock"],
+		width: (scene.assets["rock"] as g.ImageAsset).width,
+		height: (scene.assets["rock"] as g.ImageAsset).height
+	})
+	rock.x = g.game.width - rock.width;
+	rock.y = g.game.height - rock.height;
+	rock.update.add(() => 
+	{
+		rock.x --;
+		if(rock.x + rock.width < 0)
+		{
+			scene.remove(rock);
+		}
+	});
+	scene.append(rock);
+
+	return rock;
 }
 
 export = main;
